23d4aad8674ce1a48647e6430b6015f485b023ff
hzxianghaibo <cscpswang@qq.com>
2019-09-29 14:58:08 +0800

    addition for algorithm

--- src/main/java/com/hz/constantine/algorithm/dynamicplan/SimilarityOfString.java
+++ src/main/java/com/hz/constantine/algorithm/dynamicplan/SimilarityOfString.java
@@ -30,10 +30,26 @@ public class SimilarityOfString {
 
         /** fill fist row of matrix and first column of matrix **/
         for (int i = 0; i < bLen; i++) {
-            shortDistance[0][i] = i;
+            if (a.charAt(0) == b.charAt(i)) {
+                shortDistance[0][i] = i;
+            } else {
+                if (i == 0) {
+                    shortDistance[0][i] = 1;
+                } else {
+                    shortDistance[0][i] = shortDistance[0][i - 1] + 1;
+                }
+            }
         }
         for (int i = 0; i < aLen; i++) {
-            shortDistance[i][0] = i;
+            if (a.charAt(i) == b.charAt(0)) {
+                shortDistance[i][0] = i;
+            } else {
+                if (i == 0) {
+                    shortDistance[i][0] = 1;
+                } else {
+                    shortDistance[i][0] = shortDistance[i - 1][0] + 1;
+                }
+            }
         }
         /** use dynamic plan **/
         for (int i = 1; i < aLen; i++) {
@@ -51,9 +67,8 @@ public class SimilarityOfString {
     public BigDecimal similarityWithDynamicPlan(String str1, String str2) {
         int ld = shortestDistanceWithDynamicPlan(str1, str2);
         // formula is: (1 - ld / max(str1.length(), str2.length()))
-        return BigDecimal.ONE
-                .subtract(BigDecimal.valueOf(ld).divide(BigDecimal.valueOf(Math.max(str1.length(), str2.length())),2,
-                        RoundingMode.HALF_UP));
+        return BigDecimal.ONE.subtract(BigDecimal.valueOf(ld)
+                .divide(BigDecimal.valueOf(Math.max(str1.length(), str2.length())), 2, RoundingMode.HALF_UP));
     }
 
     @Test
@@ -70,16 +85,25 @@ public class SimilarityOfString {
     }
 
     @Test
-    public void test(){
+    public void test() {
         String main = "onWHiTE出七外五位为您洛感up!3%30一名世茶用美白美容液之A1T9片匕产品细节包装一盒30片装，使用时间更长3S材质面膜纸选用长纤维无纺布材质，一片富含充足精华使用说明在洁面和水乳后，取出面膜敷在脸上，10-15分钟后取下，用温水洗净即可";
         String pattern = "使用方法";
         System.out.println(main.length());
-        System.out.println(similarityWithDynamicPlan(main,"使用时间"));
-        System.out.println(similarityWithDynamicPlan(main,"使用"));
-        System.out.println(similarityWithDynamicPlan(main,"时间"));
-        System.out.println(similarityWithDynamicPlan(main,"用时"));
-        System.out.println(similarityWithDynamicPlan(main,"使用时"));
-        System.out.println(similarityWithDynamicPlan(main,"使用xx时间"));
+        System.out.println(similarityWithDynamicPlan(main, "使用时间"));
+        System.out.println(similarityWithDynamicPlan(main, "使用"));
+        System.out.println(similarityWithDynamicPlan(main, "时间"));
+        System.out.println(similarityWithDynamicPlan(main, "用时"));
+        System.out.println(similarityWithDynamicPlan(main, "使用时"));
+        System.out.println(similarityWithDynamicPlan(main, "使用xx时间"));
+    }
+
+    /**
+     * see: https://time.geekbang.org/column/article/75794 莱文斯特距离.
+     *
+     */
+    @Test
+    public void testLDP() {
+        Assert.assertEquals(shortestDistanceWithDynamicPlan("E1", "M2"), 2);
     }
 
 }
